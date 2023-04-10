9a3c596987837b268a5d8297b53e3abd7399f7cd
jghodge <jgerardhodge@gmail.com>
2023-04-10 10:16:11 -0500

    Debugging stomata_rankedNN function
    Issues arise in simulations in which stomatal files fail to initiate creating fewer NNs than rankno's expects to iterate across, provided a logic escape for this issue in the future.

--- MOM.py
+++ MOM.py
@@ -239,36 +239,46 @@ def stomata_rankedNN(Fatemap, rankedNNs, Rep='NA', distance='M', rankno=5):
 
     SCs=InFOV[InFOV['Fate']==1]
 
-    if distance=='M':
+    if (SCs.shape[0]>=rankno*2):
+        
+        NNerrors=''
 
-        for i in range(0,len(SCs)):
-            dx=np.round(np.abs(SCs.iloc[:,8]-SCs.iloc[i,8])); 
-            dy=np.round(np.abs(SCs.iloc[:,9]-SCs.iloc[i,9])); 
+        if distance=='M':
 
-            Dm=dx+dy
-            Dmr=np.sort(Dm)
+            for i in range(0,len(SCs)):
+                dx=np.round(np.abs(SCs.iloc[:,8]-SCs.iloc[i,8])); 
+                dy=np.round(np.abs(SCs.iloc[:,9]-SCs.iloc[i,9])); 
 
-            for j in range(1,rankno+1):
-                NNm=SCs.iloc[np.where(Dm==Dmr[j])[0][0],]
-                data={'Rep': Rep, 'Current_SC': i, 'NN_rank': j, 'dist':Dmr[j], 'Origin_X': SCs.iloc[i,8], 'Origin_Y': SCs.iloc[i,9], 'NN_x': NNm[8],  'NN_y': NNm[9], 'dist_xdiff': np.round(NNm[8]-SCs.iloc[i,8],2), 'dist_ydiff': np.round(NNm[9]-SCs.iloc[i,9],2)}
-                rankedNNs.loc[len(rankedNNs)] = data
-    elif distance=='E':
+                Dm=dx+dy
+                Dmr=np.sort(Dm)
 
-        for i in range(0,len(SCs)):
-            dx=np.round(np.square(SCs.iloc[:,8]-SCs.iloc[i,8])); 
-            dy=np.round(np.square(SCs.iloc[:,9]-SCs.iloc[i,9])); 
+                for j in range(1,rankno+1):
+                    NNm=SCs.iloc[np.where(Dm==Dmr[j])[0][0],]
+                    data={'Rep': Rep, 'Current_SC': i, 'NN_rank': j, 'dist':Dmr[j], 'Origin_X': SCs.iloc[i,8], 'Origin_Y': SCs.iloc[i,9], 'NN_x': NNm[8],  'NN_y': NNm[9], 'dist_xdiff': np.round(NNm[8]-SCs.iloc[i,8],2), 'dist_ydiff': np.round(NNm[9]-SCs.iloc[i,9],2)}
+                    rankedNNs.loc[len(rankedNNs)] = data
+        elif distance=='E':
 
-            Dm=np.sqrt(dx+dy)
-            Dmr=np.sort(Dm)
+            for i in range(0,len(SCs)):
+                dx=np.round(np.square(SCs.iloc[:,8]-SCs.iloc[i,8])); 
+                dy=np.round(np.square(SCs.iloc[:,9]-SCs.iloc[i,9])); 
 
-            for j in range(1,rankno+1):
-                NNm=SCs.iloc[np.where(Dm==Dmr[j])[0][0],]
-                data={'Rep': Rep, 'Current_SC': i, 'MNN_rank': j, 'Manhat_dist':Dmr[j], 'Origin_X': SCs.iloc[i,8], 'Origin_Y': SCs.iloc[i,9], 'MNN_x': NNm[8],  'MNN_y': NNm[9], 'Manhat_dist_xdiff': np.round(NNm[8]-SCs.iloc[i,8],2), 'Manhat_dist_ydiff': np.round(NNm[9]-SCs.iloc[i,9],2)}
-                rankedNNs.loc[len(rankedNNs)] = data
+                Dm=np.sqrt(dx+dy)
+                Dmr=np.sort(Dm)
+
+                for j in range(1,rankno+1):
+                    NNm=SCs.iloc[np.where(Dm==Dmr[j])[0][0],]
+                    data={'Rep': Rep, 'Current_SC': i, 'MNN_rank': j, 'Manhat_dist':Dmr[j], 'Origin_X': SCs.iloc[i,8], 'Origin_Y': SCs.iloc[i,9], 'MNN_x': NNm[8],  'MNN_y': NNm[9], 'Manhat_dist_xdiff': np.round(NNm[8]-SCs.iloc[i,8],2), 'Manhat_dist_ydiff': np.round(NNm[9]-SCs.iloc[i,9],2)}
+                    rankedNNs.loc[len(rankedNNs)] = data
+        else:
+            print('Distance method supplied not recognized. Present options are \'M\' for Manhattan (Default) and \'E\' for Euclidean.')
+            
+
+        return NNerrors, rankedNNs 
     else:
-        print('Distance method supplied not recognized. Present options are \'M\' for Manhattan (Default) and \'E\' for Euclidean.')
 
-    return rankedNNs
+        NNerrors='NNs_scarce_fatal_run'
+        
+        return NNerrors, rankedNNs
 
 def stomatagenesis(VeinMu, VeinVar, MesoWgt, MesoVar, SigMaxV, SigMaxM, SigVar, SFMu, SFVar, SFEntropy, VeinFileMu, VeinFileVar, MesoFileMu, MesoFileVar, CPLenMu, CPLenVar, CPLenCov, PLenMu, PLenVar, PLenCov, GCLenMu, GCLenVar, GCLenCov, AsymLenMu, AsymLenVar, AsymLenCov, Yskew=3):
     
